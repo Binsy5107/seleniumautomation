7bcf30ca7bad527e0c3d739362b742f07d3a324a
Bincy <Bincy@binsy.bbrouter>
2025-05-21 20:48:10 +0530

    second selenium commit

--- AutomtionCourse/src/test/java/seleniumBasics/HandlingLocators.java
+++ AutomtionCourse/src/test/java/seleniumBasics/HandlingLocators.java
@@ -2,7 +2,8 @@ package seleniumBasics;
 
 import org.openqa.selenium.By;
 
-public class HandlingLocators extends Base{
+public class HandlingLocators extends Base {
+	
 	public void verifyLocators()
 	{
 		driver.findElement(By.id("button-one"));
@@ -10,16 +11,27 @@ public class HandlingLocators extends Base{
 		driver.findElement(By.tagName("button"));
 		driver.findElement(By.name("viewport"));
 		driver.findElement(By.linkText("Simple Form Demo"));
-		driver.findElement(By.partialLinkText("Simple Form "));
+		driver.findElement(By.partialLinkText("Simple Form"));
 		driver.findElement(By.cssSelector("input[id='single-input-field']"));
-	}
+		driver.findElement(By.xpath("//button[@id='button-one']"));
+		driver.findElement(By.xpath("//button[text()='Show Message']"));
+		driver.findElement(By.xpath("//button[starts-with(text(),'Show ')"));
+		driver.findElement(By.xpath("//button[@id='button-one' and @type='button'])"));
+		driver.findElement(By.xpath("//button[@id='button-one' or @id='button-one-electronics']"));
+		driver.findElement(By.xpath("//div[contains (text(), 'Single Input Field')]//parent::div[@class='card']"));
+		driver.findElement(By.xpath("//div[@class='card']//child::button[@id='button-one']"));
+		driver.findElement(By.xpath("//button[@id='button-one']//following::div[@class='card']"));
+		driver.findElement(By.xpath("//button[@id='button-one']//ancestor::div"));
+		driver.findElement(By.xpath("//div[@class='card']//descendant::div"));
+				}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-HandlingLocators locators=new HandlingLocators();
-locators.intializeBrowser();
-locators.verifyLocators();
-//locators.browserClose();
+		HandlingLocators locators=new HandlingLocators();
+		locators.intializeBrowser();
+		locators.verifyLocators();
+		//locators.browserClose();
 	}
 
 }
---
+++ AutomtionCourse/src/test/java/seleniumBasics/HandlingWebElementCommands.java
@@ -0,0 +1,32 @@
+package seleniumBasics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HandlingWebElementCommands extends Base {
+	public void verifywebelementcommands()
+	{driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
+	WebElement messagebox=driver.findElement(By.xpath("//input[@id='single-input-field']"));
+		// to input a value sendkeys.
+	messagebox.sendKeys("binsy");
+	// to click the button element.
+	WebElement showMessageButton=driver.findElement(By.xpath("//button[@id='button-one']"));
+	showMessageButton.click();
+	WebElement yourMessage=driver.findElement(By.xpath("//div[@id='message-one']"));
+	System.out.println(yourMessage.getText());
+	messagebox.clear();
+	WebElement valuea=driver.findElement(By.id("value-a"));
+	System.out.println(valuea.getTagName());
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+HandlingWebElementCommands webelements= new HandlingWebElementCommands();
+webelements.intializeBrowser();
+webelements.verifywebelementcommands();
+//webelements.browserClose();
+
+
+	}
+
+}
